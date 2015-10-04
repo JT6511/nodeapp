e1d7e5978d570311eec1d49fd98fb12b1c8a34ac
Jon Thomas <jonthomas@lillymac-jonthomas.attlocal.net>
2015-10-04 09:07:48 -0400

    Add Hello page

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+// Configure our HTTP server to respond with Hello World to all requests.
+app.get('/hello', function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World - JT customied this line!\n");
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
